5b31adb8d0b6ea3a4563db3242e378ecf505f041
fletgas <fletgas@g.educaand.es>
2024-10-01 19:49:25 +0200

    ejercicios 1

--- src/ej03.py
+++ src/ej03.py
@@ -1,6 +1,10 @@
 ancho = 17
 alto = 12.0
 a = ancho / 2
+print("ancho / 2 = " + type(ancho/2))
+print("ancho // 2 = " + type(ancho//2))
+ 
+a = ancho / 2
 b = ancho // 2
 c = alto / 3
 d = a + b * 5
---
+++ src/ej13.py
@@ -0,0 +1,6 @@
+n = int(input("Introduce el primer número: "))
+m = int(input("Introduce el segundo número: "))
+c = n / m
+r = n % m
+print(f"La división de {n} entre {m} da un cociente {c} y un resto {r}")
+
